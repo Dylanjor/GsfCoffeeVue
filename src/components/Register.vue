259060e8ecd3ded37d131f76518435d53fd28ef1
礼家 代 <2545664565@qq.com>
2020-04-16 15:17:07 +0800

    updatezhushi

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -4,198 +4,197 @@
 </style>
 <template>
 <div>
+  <!-- 总的用户table -->
    <el-table
-   :data="tableData.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"
-    border
-    stripe
-    style="width: 100%">
-    <el-table-column
-      type="index"
-      label="序号"
-      fixed
-      :index="indexMethod">
-    </el-table-column>
-    <el-table-column
-      label="id"
-      fixed
-      prop="id"
-      align="center"
-      width="80">
-    </el-table-column>
-    <el-table-column
-      label="名字"
-      fixed
-      prop="name"
-      align="center"
-      width="80">
-    </el-table-column>
-    <el-table-column
-      label="密码"
-      prop="passWord"
-      align="center"
-      width="80"
-      >
-    </el-table-column>
-    <el-table-column
-      label="性别"
-      prop="resource"
-      align="center"
-      width="80"
-      >
-    </el-table-column>
-    <el-table-column
-      label="生日"
-      prop="date"
-      align="center"
-      width="200"
-      >
-    </el-table-column>
-    <el-table-column
-      label="是否是会员"
-      prop="delivery"
-      align="center"
-      width="100"
-      >
-    </el-table-column>
-     <el-table-column
-      label="爱好"
-      prop="type"
+    :data="tableData.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"
+      border
+      stripe
+      style="width: 100%">
+      <el-table-column
+        type="index"
+        label="序号"
+        fixed
+        :index="indexMethod">
+      </el-table-column>
+      <el-table-column
+        label="id"
+        fixed
+        prop="id"
         align="center"
-      width="120"
-      >
-    </el-table-column>
-    <el-table-column
-      label="注册时间"
-      prop="createTime"
-      sortable
-      align="center"
-      width="230">
-    </el-table-column>
-    <el-table-column
-      label="最近一次修改时间"
-      prop="updateTime"
-      sortable
-      align="center"
-      width="240"
-       >
-    </el-table-column>
-     <el-table-column
-      label="电话"
-      prop="tel"
-      align="center"
-      width="140"
-      >
-    </el-table-column>
-    <el-table-column
-      label="账号"
-      prop="numbering"
-      align="center"
-      width="140"
-      >
-    </el-table-column>
-    <el-table-column
-      label="是否弃用"
-      prop="deprecated"
-      align="center"
-      width="100"
-      >
-    </el-table-column>
-    <el-table-column
-      label="弃用时间"
-      prop="deprecatedTime"
-      align="center"
-      width="100"
-      >
-    </el-table-column>
-    <el-table-column
-     fixed="right"
-     width="150"
-     align="right">
-      <template slot="header">
-        <el-input
-          v-model="search"
-          size="mini"
-          placeholder="输入关键字搜索"/>
-      </template>
-      <template slot-scope="scope">
-       <el-button
-       size="mini"
-        @click="getshoppingDetails(scope.row)">购物</el-button>
-       <el-button
-        size="mini"
-        type="danger"
-        @click="handleDelete(scope.$index, scope.row)">删除</el-button>
-      </template>
-    </el-table-column>
-
-  </el-table>
-
-  <el-dialog title="购物车" :visible.sync="dialogFormVisible">
-  <el-table
-    :data="this.gridData"
-    stripe
-     max-height="300"
-    style="width: 100%">
-    <el-table-column
-      type="index"
-      fixed
-      width="50"
-     align="center"
-      :index="indexMethod">
-    </el-table-column>
-    <el-table-column
-      label="商品ID"
-      prop="commodityId"
-      width="100"
-      align="center">
-    </el-table-column>
-    <el-table-column
-      label="数量"
-      prop="qty"
-      width="50"
-      align="center">
-    </el-table-column>
-     <el-table-column
-      label="创建时间"
-      prop="createTime"
-       width="200"
-      align="center">
-    </el-table-column>
-    <el-table-column
-      label="是否已购买"
-      prop="isBought"
-      width="150"
-      align="center">
-    </el-table-column>
-    <el-table-column
-      label="购买时间"
-      prop="boughtTime"
-      width="200"
-      align="center">
-    </el-table-column>
-     <el-table-column
-      label="创建人"
-      prop="founderId"
-      width="100"
-      align="center">
-    </el-table-column>
-  <el-table-column
+        width="80">
+      </el-table-column>
+      <el-table-column
+        label="名字"
+        fixed
+        prop="name"
+        align="center"
+        width="80">
+      </el-table-column>
+      <el-table-column
+        label="密码"
+        prop="passWord"
+        align="center"
+        width="80"
+        >
+      </el-table-column>
+      <el-table-column
+        label="性别"
+        prop="resource"
+        align="center"
+        width="80"
+        >
+      </el-table-column>
+      <el-table-column
+        label="生日"
+        prop="date"
+        align="center"
+        width="200"
+        >
+      </el-table-column>
+      <el-table-column
+        label="是否是会员"
+        prop="delivery"
+        align="center"
+        width="100"
+        >
+      </el-table-column>
+      <el-table-column
+        label="爱好"
+        prop="type"
+          align="center"
+        width="120"
+        >
+      </el-table-column>
+      <el-table-column
+        label="注册时间"
+        prop="createTime"
+        sortable
+        align="center"
+        width="230">
+      </el-table-column>
+      <el-table-column
+        label="最近一次修改时间"
+        prop="updateTime"
+        sortable
+        align="center"
+        width="240"
+        >
+      </el-table-column>
+      <el-table-column
+        label="电话"
+        prop="tel"
+        align="center"
+        width="140"
+        >
+      </el-table-column>
+      <el-table-column
+        label="账号"
+        prop="numbering"
+        align="center"
+        width="140"
+        >
+      </el-table-column>
+      <el-table-column
+        label="是否弃用"
+        prop="deprecated"
+        align="center"
+        width="100"
+        >
+      </el-table-column>
+      <el-table-column
+        label="弃用时间"
+        prop="deprecatedTime"
+        align="center"
+        width="100"
+        >
+      </el-table-column>
+      <el-table-column
       fixed="right"
-      width="100"
-      align="center"
-      label="操作"
-     >
-      <template slot-scope="scope">
-        <el-button @click="handleClick(scope.row)" type="text" size="small">编辑</el-button>
-        <el-button type="text" size="small">删除</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
-<div slot="footer" class="dialog-footer">
-    <el-button @click="dialogFormVisible = false">取 消</el-button>
-</div>
-
-</el-dialog>
+      width="150"
+      align="right">
+        <template slot="header">
+          <el-input
+            v-model="search"
+            size="mini"
+            placeholder="输入关键字搜索"/>
+        </template>
+        <template slot-scope="scope">
+        <el-button
+        size="mini"
+          @click="getshoppingDetails(scope.row)">购物</el-button>
+        <el-button
+          size="mini"
+          type="danger"
+          @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  <!-- 弹出个人的购物车信息 -->
+    <el-dialog title="购物车" :visible.sync="dialogFormVisible">
+      <el-table
+        :data="this.gridData"
+        stripe
+        max-height="300"
+        style="width: 100%">
+        <el-table-column
+          type="index"
+          fixed
+          width="50"
+        align="center"
+          :index="indexMethod">
+        </el-table-column>
+        <el-table-column
+          label="商品ID"
+          prop="commodityId"
+          width="100"
+          align="center">
+        </el-table-column>
+        <el-table-column
+          label="数量"
+          prop="qty"
+          width="50"
+          align="center">
+        </el-table-column>
+        <el-table-column
+          label="创建时间"
+          prop="createTime"
+          width="200"
+          align="center">
+        </el-table-column>
+        <el-table-column
+          label="是否已购买"
+          prop="isBought"
+          width="150"
+          align="center">
+        </el-table-column>
+        <el-table-column
+          label="购买时间"
+          prop="boughtTime"
+          width="200"
+          align="center">
+        </el-table-column>
+        <el-table-column
+          label="创建人"
+          prop="founderId"
+          width="100"
+          align="center">
+        </el-table-column>
+        <el-table-column
+          fixed="right"
+          width="100"
+          align="center"
+          label="操作"
+        >
+          <template slot-scope="scope">
+            <el-button @click="handleClick(scope.row)" type="text" size="small">编辑</el-button>
+            <el-button type="text" size="small">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+      </div>
+    </el-dialog>
 </div>
 </template>
 
