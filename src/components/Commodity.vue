b2ee91eed03c4272be0869fb3f1f24b28c769649
礼家 代 <2545664565@qq.com>
2020-05-05 11:16:22 +0800

    商品信息管理删除ok

--- src/components/Commodity.vue
+++ src/components/Commodity.vue
@@ -20,20 +20,24 @@
       <el-table-column label="售价" prop="sellingPrice" align="center" :formatter="dateFormatSelling"></el-table-column>
       <el-table-column label="描述" prop="desc" align="center"> </el-table-column>
       <el-table-column label="注册时间" prop="createTime" align="center" :formatter="dateFormat" sortablealign="center"></el-table-column>
-      <el-table-column fixed="right"  align="center">
-          <el-button size="mini">修改</el-button>
-          <el-button size="mini" type="danger">删除</el-button>
-      </el-table-column>
+        <el-table-column fixed="right"  align="center">
+          <template slot-scope="scope">
+            <el-button size="mini">修改</el-button>
+            <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+            </template>
+        </el-table-column>
     </el-table>
 </div>
 </template>
 <script lang="js">
 // eslint-disable-next-line no-unused-vars
 // eslint-disable-next-line standard/object-curly-even-spacing
-import {Loading } from 'element-ui'
+// eslint-disable-next-line no-unused-vars
+import { Loading, Message } from 'element-ui'
 import moment from 'moment'
 
 export default({
+  inject: ['reload'],
   name: 'CommModity',
   data: function () {
     return {
@@ -93,14 +97,35 @@ export default({
         }
       })
     },
-    dateFormat: function (row, column) {
+    dateFormat (row, column) {
       const date = new Date(row[column.property])
       if (date === undefined) { return '' }
       return moment(date).format('YYYY-MM-DD')
     },
-    dateFormatSelling: function (row, column) {
+    dateFormatSelling (row, column) {
       const Selling = row[column.property]
       return Selling + '元'
+    },
+    handleDelete (index, row) {
+      var that = this
+      this.$confirm('此操作将永久删除此信息, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        that.$axios({
+          method: 'GET',
+          url: that.api.baseURL + 'Commodity/DeleteComm?CommId=' + row.id
+        }).then((response) => { // 这里使用了ES6的语法
+          that.$message({
+            type: 'success',
+            message: '删除成功'
+          })
+          that.reload()
+        }).catch((error) =>
+          console.log(error) // 请求失败返回的数据
+        )
+      })
     }
   },
   created () {
